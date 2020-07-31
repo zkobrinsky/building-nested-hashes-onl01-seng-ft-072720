16e4f9cacc9cd3e2b869144aa7bd53c10e7c8a9a
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-31 19:12:55 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,16 +2,17 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        name: "Lord Montague",
+        age: "53"
         },
       :matriarch => {
-        #your key/value pairs here
+
         },
       :hero => {
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -26,5 +27,5 @@ def third_challenge
    }
   }
 
-  
+
 end
