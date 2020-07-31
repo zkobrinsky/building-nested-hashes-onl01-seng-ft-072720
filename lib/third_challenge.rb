5c1864cf51b691288212621685c179f73e0597d1
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-31 19:14:55 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -26,7 +26,10 @@ def third_challenge
         age: "51"
         },
       :heroine => {
-        #your key/value pairs here
+        name: "Juliet", 
+        age: "15",
+        status: "alive"
+      
         },
       :heroine_friends => []
    }
