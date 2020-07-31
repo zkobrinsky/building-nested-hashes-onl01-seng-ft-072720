e45638a263a5aeefaddfddb2b4b39010d747236b
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-31 19:18:30 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -23,9 +23,9 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        {
-
-        }
+        # {
+        #
+        # }
         #your hashes here!
       ]
    }
