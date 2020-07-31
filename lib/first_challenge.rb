d0c9aa4d0a2706b9196493289db35645a31a365d
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-31 19:07:59 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,16 +1,33 @@
+# def first_challenge
+#    epic_tragedy = {
+#      montague: {
+#        patriarch: {},
+#        matriarch: {},
+#        hero: {},
+#        hero_friends: []
+#      },
+#      capulet: {
+#        patriarch: {},
+#        matriarch: {},
+#        heroine: {},
+#        heroine_friends: []
+#      }
+#    }
+# end
+
 def first_challenge
    epic_tragedy = {
-     montague: {
-       patriarch: {},
-       matriarch: {},
-       hero: {},
-       hero_friends: []
+     :montague => {
+       :patriarch => {},
+       :matriarch => {},
+       :hero => {},
+       :hero_friends => []
      },
-     capulet: {
-       patriarch: {},
-       matriarch: {},
-       heroine: {},
-       heroine_friends: []
+     :capulet => {
+       :patriarch => {},
+       :matriarch => {},
+       :heroine => {},
+       :heroine_friends => []
      }
    }
 end
