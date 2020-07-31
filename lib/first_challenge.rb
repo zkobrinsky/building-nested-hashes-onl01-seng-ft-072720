db7de6c8edb0141f013ebd0e68c9938fe4e4e6a1
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-31 19:05:01 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -6,6 +6,11 @@ def first_challenge
        hero: {}
        hero_friends: []
      }
-     capulet: {}
+     capulet: {
+       patriarch: {}
+       matriarch: {}
+       heroine: {}
+       heroine_friends: []
+     }
    }
 end
